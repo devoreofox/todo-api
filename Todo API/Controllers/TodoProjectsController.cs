1a0e5637c8ac0fb485b9982c4bef574741ec5f63
Oreo <232652342+devoreofox@users.noreply.github.com>
2026-02-11 22:45:48 -0400

    Made initial TodoProject controller with simple HelloWorld endpoint.

---
+++ Todo API/Controllers/TodoProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/list")]
+    public class TodoProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
