47dc2293f9c33f40820da9e19f0982f517d72ee4
Oreo <232652342+devoreofox@users.noreply.github.com>
2026-02-11 21:34:37 -0400

    Added Models, and TodoItem Model.

---
+++ Todo API/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Todo_API.Models
+{
+    public record TodoItem
+    {
+        public Guid Id { get; init; }
+        public string Title { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public bool IsComplete { get; init; }
+        public DateTimeOffset? DueDate { get; init; }
+        public DateTimeOffset? CreatedAt { get; init; }
+        public DateTimeOffset? UpdatedAt { get; init; }
+    }
+}
