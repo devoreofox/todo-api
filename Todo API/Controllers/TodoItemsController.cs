2aee5ec0cf7908ee315c6af3b1ed3b194d8caf81
Oreo <232652342+devoreofox@users.noreply.github.com>
2026-02-14 16:16:41 -0400

    Added controller for TodoItems, and added GET endpoint to that with dummy helloworld response.

---
+++ Todo API/Controllers/TodoItemsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/list/{projectId:guid}/items")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
